4942f7cbf61fe64ed72646c85e3a52da74c875c3
amagana8 <alexander.jm28@gmail.com>
2024-10-18 03:10:05 +0000

    graceful shutdown

--- question-service/application/app.go
+++ question-service/application/app.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+	"time"
 
 	"github.com/amagana8/trivia-games/middleware"
 	"go.mongodb.org/mongo-driver/mongo"
@@ -30,12 +31,35 @@ func (a *App) Run(ctx context.Context) error {
 		Handler: middleware.Logger(loadRoutes()),
 	}
 
+	// deferring so I don't have to repeat this in every return case
+	//  and wrapping in anonymous function since disconnect returns an error
+	defer func() {
+		err := a.db.Disconnect(ctx)
+		if err != nil {
+			fmt.Println("failed to close redis", err)
+		}
+	}()
+
 	log.Println("Starting server")
 
-	err := server.ListenAndServe()
-	if err != nil {
-		return fmt.Errorf("failed to start server: %w", err)
-	}
+	ch := make(chan error, 1)
 
-	return nil
+	go func() {
+		err := server.ListenAndServe()
+		if err != nil {
+			ch <- fmt.Errorf("failed to start server: %w", err)
+		}
+		close(ch)
+	}()
+
+	select {
+	case err := <-ch:
+		return err
+	case <-ctx.Done():
+		// give time for any ongoing requests to finish
+		timeout, cancel := context.WithTimeout(context.Background(), time.Second*10)
+		defer cancel()
+
+		return server.Shutdown(timeout)
+	}
 }
--- question-service/main.go
+++ question-service/main.go
@@ -4,6 +4,8 @@ import (
 	"context"
 	"flag"
 	"fmt"
+	"os"
+	"os/signal"
 
 	"github.com/amagana8/trivia-games/application"
 	"go.mongodb.org/mongo-driver/mongo"
@@ -13,15 +15,17 @@ import (
 func main() {
 	mongoURI := flag.String("mongoURI", "mongodb://localhost:27017", "Database hostname url")
 
-	client, err := mongo.Connect(context.TODO(), options.Client().
-		ApplyURI(*mongoURI))
+	ctx, cancel := signal.NotifyContext(context.Background(), os.Interrupt)
+	defer cancel()
+
+	client, err := mongo.Connect(ctx, options.Client().ApplyURI(*mongoURI))
 	if err != nil {
 		fmt.Println("failed to connect to mongodb: %w", err)
 	}
 
 	app := application.New(":3001", client)
 
-	err = app.Run(context.TODO())
+	err = app.Run(ctx)
 	if err != nil {
 		fmt.Println("failed to start app:", err)
 	}
