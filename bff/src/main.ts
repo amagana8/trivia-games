4d701e9ad647ce0a2c39c1e7a186a71f6258dd0d
amagana8 <alexander.jm28@gmail.com>
2024-11-13 04:27:19 +0000

    fix(bff): no more cors issues

--- bff/src/main.ts
+++ bff/src/main.ts
@@ -2,8 +2,13 @@ import { fastifyTRPCPlugin } from "@trpc/server/adapters/fastify";
 import fastify from "fastify";
 import { appRouter } from "./router/index.js";
 import { createContext } from "./router/context.js";
+import cors from "@fastify/cors"
 
 const server = fastify({ logger: true });
+
+server.register(cors, {
+  origin: "http://localhost:5173",
+});
 
 server.register(fastifyTRPCPlugin, {
   prefix: "/trpc",
